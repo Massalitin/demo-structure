d5f1489361ebffad00a3f9edc0f464d23394d8c7
Massalitin Dmytro <massalitin.dmytro@zultys.com>
2023-08-23 14:19:19 +0300

    working on demo

---
+++ src/components/nav-sidebar/nav-sidebar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavSidebarComponent } from './nav-sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavSidebarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NavSidebarModule { }
